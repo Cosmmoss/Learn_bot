a232f523da5596a6afb2c85c98cd5ce1e8fa81f6
Cosmmoss <pavelandreevich16@gmail.com>
2024-05-28 14:28:40 +0300

    Changed 8th task - bot

--- bot.py
+++ bot.py
@@ -1,22 +1,60 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-logging.basicConfig(filename = 'bot.log', level = logging.INFO)
+import ephem
 import settings
+import locale
+from datetime import datetime
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log')
+
+planets = {"Mercury": ephem.Mercury, 
+            "Venus": ephem.Venus, 
+            "Mars" : ephem.Mars, 
+            "Saturn": ephem.Saturn, 
+            "Uranus": ephem.Uranus, 
+            "Neptune": ephem.Neptune,
+            "Pluto": ephem.Pluto,
+            "Moon": ephem.Moon,
+            "Sun": ephem.Sun,
+            "Jupiter": ephem.Jupiter
+            }
 
 def greet_user(update, context):  # update - это то, что поступило от пользователя Telegram. Context - это спец. штука с помощью которой мы можем изнутри функции отдавать команды боту
     print('Вызван /start')
     print(update.message.reply_text('Здравствуй пользователь!'))
 
-def talk_to_me(update, context):
-    text = update.message.text
-    print(text)
-    update.message.reply_text(text)
+def planet_const(update, context):  # вызов команды /planet
+    print('Вызван /planet')
+    update.message.reply_text("Введите название объекта Солнечной системы -\
+ Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto, Moon, Sun")
+    
+def get_constelletion(planet_name):  # функция обрабатывает сообщение пользователя с названием планеты
+    locale.setlocale(locale.LC_TIME, 'ru_RU')
+    current_date = datetime.now().strftime('%d.%m.%Y %H:%M')
+    current_planet = planets[planet_name](current_date)
+    current_date = datetime.now().strftime('%A %d.%m.%Y %H:%M')  # переменная с отображением дня недели на кириллице
+    print(current_planet, current_date, sep='\n')
+    return ephem.constellation(current_planet), current_date
 
+def talk_to_me(update, context):  # функция принимает и распределяет все сообщения от пользователя
+    user_text = update.message.text
+    print(user_text)
+    if user_text.lower().capitalize() in planets:
+        planet_name = user_text
+        const, current_date = get_constelletion(planet_name)  # получаем переменные из функции get_constelletion(planet_name)
+        update.message.reply_text(f"Сегодня: {current_date}")
+        update.message.reply_text(f"Планета {planet_name} в созвездии: {const[1]}")
+        print(const)
+    else:
+        update.message.reply_text(f"Хотите узнать про созвездия, нажмите: {'/planet'}")
+    
 def main():
     mybot = Updater(settings.API_KEY, use_context = True)
     
     dp = mybot.dispatcher  # в переменную dp кладём это, чтобы в дальнейшем не набирать длинное название
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet_const))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     logging.info("Bot started")
